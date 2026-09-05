d3fa23486a3ec5894223c9027ca548e8cdc9d8cd
trunets <nattavee812@gmail.com>
2026-09-05 19:06:04 +0700

    Added sample seed data

---
+++ seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (name) VALUES ('Food');
+INSERT INTO categories (name) VALUES ('Transport');
+INSERT INTO categories (name) VALUES ('Entertainment');
+
+INSERT INTO expenses (description, amount, category_id, date) VALUES ('Lunch at cafe', 8.50, 1, '2026-09-01');
+INSERT INTO expenses (description, amount, category_id, date) VALUES ('Bus fare', 2.50, 2, '2026-09-01');
+INSERT INTO expenses (description, amount, category_id, date) VALUES ('Basketball match ticket', 8.50, 3, '2026-09-02');
+INSERT INTO expenses (description, amount, category_id, date) VALUES ('Groceries', 8.50, 1, '2026-09-04');
