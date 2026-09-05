b3df8139d2533f1a1fd98bd19ed7dd098281265a
trunets <nattavee812@gmail.com>
2026-09-05 21:00:38 +0700

    Additional queries

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- Total spending per category
-select categories.name, sum(expenses.amount) as total_amount
+select categories.name, sum(expenses.amount) as total_expense
 from expenses
 join categories on expenses.category_id = categories.id
 group by categories.name;
@@ -8,3 +8,20 @@ group by categories.name;
 select sum(amount) as September_spendings
 from expenses
 where strftime('%Y-%m', date) = '2026-09';
+
+-- Most Expensive single purchase
+select description,amount 
+from expenses
+order by amount desc limit 1;
+
+--Average expense per category
+select categories.name, avg(expenses.amount) as avg_expense
+from expenses
+join categories on expenses.category_id = categories.id
+group by categories.name;
+
+-- Count how many expenses per category
+select categories.name, count(*) as expense_per_category
+from expenses
+join categories on expenses.category_id = categories.id
+group by categories.name;
