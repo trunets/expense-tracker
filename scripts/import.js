76fe93e4ee3eafb7f2c5750c77f10069d1863fe6
trunets <nattavee812@gmail.com>
2026-09-06 13:36:36 +0700

    Add nodejs scripts to insert data through CSV

---
+++ scripts/import.js
@@ -0,0 +1,31 @@
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+const db = new sqlite3.Database('../expenses.db');
+
+const csvContent = fs.readFileSync('expenses.csv');
+
+parse(csvContent, {
+  columns: true,  // treats the first row as headers
+  skip_empty_lines: true
+}, (err, rows) => {
+  if (err) {
+    console.error('Error parsing CSV:', err);
+    return;
+  }
+
+  rows.forEach((row) => {
+    db.run(
+        'insert into expenses (description,amount,category_id, date) values(?,?,?,?)',
+        [row.description,row.amount,row.category_id,row.date],
+        (err) => {
+            if(err){
+                console.error('Error inserting row: ',err);
+            }else {
+                console.log('Inserted: ',row.description);
+            }
+        }
+    )
+  })
+});
